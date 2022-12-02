9fa6541a1463fd95ba3c60af2a0c9aa0674c0e98
codawi <codawi358@gmail.com>
2022-12-02 19:55:20 +0900

    add:管理者用設定値のSeederファイル作成

--- database/seeders/DeliverySettingSeeder.php
+++ database/seeders/DeliverySettingSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DeliverySettingSeeder extends Seeder
 {
@@ -14,16 +15,23 @@ class DeliverySettingSeeder extends Seeder
      */
     public function run()
     {
-        $params = 
-        [
-            //最短配送日翌日、選択肢5、15以降でもずらさない、土日配送可、北海道最短+2日沖縄最短+3日
-            [
+        DB::table('delivery_settings')->insert([
+                //最短配送日翌日、選択肢5、15以降でも翌日配送、土日配送可、北海道最短+2日沖縄最短+3日の設定値
+                [
                 'shortest_delivery_dates' => '1',
                 'options' => '5',
                 'change_delivery_dates' => '0',
                 'delivery_not_possible' => '0',
-                'prefecture' => '0'
-            ]
-        ]
+                'prefecture' => '1'
+                ],
+                //最短配送日明後日、選択肢10、15時以降最短+1日、土日配送不可、北海道+3日沖縄+3日の設定値
+                [
+                    'shortest_delivery_dates' => '2',
+                'options' => '10',
+                'change_delivery_dates' => '1',
+                'delivery_not_possible' => '1',
+                'prefecture' => '2'
+                ]
+            ]);
     }
 }
--- resources/views/order_form.blade.php
+++ resources/views/order_form.blade.php
@@ -6,5 +6,10 @@
         <title>注文フォーム</title>
         @vite('resources/js/app.js')
   </head>
+  <div>
+    <label>お届け先
+      <select name="" id=""></select>
+    </label>
+  </div>
   </body>
 </html>
